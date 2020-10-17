0449a335275af231e588d0de7422212c65b4aa1d
Tyrel Chambers <tychambers3@gmail.com>
2020-10-17 11:45:20 -0400

    downloadspinner

---
+++ src/components/Spinner/Spinner.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./Spinner.css";
+const Spinner = () => {
+  return <div class="loader"></div>;
+};
+
+export default Spinner;
--- src/pages/NewProject/NewProject.jsx
+++ src/pages/NewProject/NewProject.jsx
@@ -1,12 +1,54 @@
 import React from "react";
+import { useState } from "react";
 import { MainButton } from "../../components/Buttons/Buttons";
 import { H2 } from "../../components/Headings/Headings";
+import Spinner from "../../components/Spinner/Spinner";
 import ProjectForm from "../../forms/ProjectForm";
 import DisplayWrapper from "../../layouts/DisplayWrapper/DisplayWrapper";
 
 const str = `npx create-react-app project-name`;
 
 const NewProject = () => {
+  const [downloading, setDownloading] = useState(false);
+
+  const FinalScreen = () => (
+    <div className="flex flex-col overflow-hidden">
+      <div className="flex flex-col items-center p-4 pt-8 pb-8">
+        <p className="font-bold text-lg mb-4">Download your project files</p>
+
+        {downloading && (
+          <div className="flex items-center mt-4">
+            <Spinner />
+            <p className="text-pink-500 font-bold ml-4">Preparing files...</p>
+          </div>
+        )}
+
+        {!downloading && (
+          <MainButton>
+            <i className="fas fa-cloud-download-alt mr-4 "></i>Download
+          </MainButton>
+        )}
+      </div>
+      <div className="flex flex-col items-center p-4 pt-8 pb-8 bg-gray-900 ">
+        <p className="font-bold text-white text-lg mb-4">
+          Use the command terminal
+        </p>
+
+        <div className="p-4 bg-gray-800 w-full rounded-md flex justify-center">
+          <p className="text-white flex items-center">
+            <span
+              className="text-sm text-pink-500 mr-2"
+              style={{ userSelect: "none" }}
+            >
+              ${" "}
+            </span>
+            {str}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+
   return (
     <DisplayWrapper>
       <H2>New Project</H2>
@@ -14,33 +56,5 @@ const NewProject = () => {
     </DisplayWrapper>
   );
 };
-
-const FinalScreen = () => (
-  <div className="flex flex-col overflow-hidden">
-    <div className="flex flex-col items-center p-4 pt-8 pb-8">
-      <p className="font-bold text-lg mb-4">Download your project files</p>
-      <MainButton>
-        <i className="fas fa-cloud-download-alt mr-4 "></i>Download
-      </MainButton>
-    </div>
-    <div className="flex flex-col items-center p-4 pt-8 pb-8 bg-gray-900 ">
-      <p className="font-bold text-white text-lg mb-4">
-        Use the command terminal
-      </p>
-
-      <div className="p-4 bg-gray-800 w-full rounded-md flex justify-center">
-        <p className="text-white flex items-center">
-          <span
-            className="text-sm text-pink-500 mr-2"
-            style={{ userSelect: "none" }}
-          >
-            ${" "}
-          </span>
-          {str}
-        </p>
-      </div>
-    </div>
-  </div>
-);
 
 export default NewProject;
