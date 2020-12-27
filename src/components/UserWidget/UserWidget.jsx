fe51406cc1e6c9151c2ae88264aa56733f7bba79
Tyrel Chambers <tychambers3@gmail.com>
2020-12-26 20:50:02 -0500

    adjust user widget

--- src/components/UserWidget/UserWidget.jsx
+++ src/components/UserWidget/UserWidget.jsx
@@ -3,13 +3,14 @@ import React from "react";
 import "./UserWidget.css";
 const UserWidget = ({ UserStore }) => {
   return (
-    <div className="flex items-center user-widget-wrapper p-4">
-      <img
+    <div className="flex flex-col user-widget-wrapper p-4">
+      {/* <img
         src="https://images.unsplash.com/photo-1528892952291-009c663ce843?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=896&q=80"
         alt=""
         className="avatar"
-      />
-      <p className="text-white font-bold ml-4">{UserStore.user.name}</p>
+      /> */}
+      <p>Signed in as</p>
+      <p className="text-white font-bold text-2xl">{UserStore.user.name}</p>
     </div>
   );
 };
--- src/forms/ProfileSetupForm.jsx
+++ src/forms/ProfileSetupForm.jsx
@@ -1,13 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import FormLabel from "../components/FormLabel/FormLabel";
-
+import { MainButton } from "../components/Buttons/Buttons";
+import { getAxios } from "../api/index";
 const ProfileSetupForm = () => {
+  const [state, setState] = useState({
+    name: "",
+  });
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    await getAxios({
+      url: "/user/update",
+      method: "patch",
+      data: state,
+    });
+
+    window.location.pathname = "/";
+  };
   return (
     <form className="form">
       <div className="field-group">
         <FormLabel text="Name" name="name" />
-        <input type="text" className="form-input" placeholder="John Smith" />
+        <input
+          type="text"
+          className="form-input"
+          placeholder="John Smith"
+          value={state.name}
+          onChange={(e) => setState({ ...state, name: e.target.value })}
+        />
       </div>
+      <MainButton default onClick={(e) => submitHandler(e)}>
+        Save &amp; Continue
+      </MainButton>
     </form>
   );
 };
