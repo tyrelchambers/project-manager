8a2c8b2297e961d7c526351d1c00bf746dbaeb26
Tyrel Chambers <tychambers3@gmail.com>
2021-01-07 11:56:02 -0500

    circleci test

--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -4,7 +4,7 @@ import { H1 } from "../../components/Headings/Headings";
 const Header = () => {
   return (
     <header className="header flex items-center p-4">
-      <H1>Kanlen</H1>
+      <H1>Kanlen!</H1>
     </header>
   );
 };
