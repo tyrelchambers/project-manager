123b8d8713f9f2e7d320b686adb266a12ce4115c
Tyrel Chambers <tychambers3@gmail.com>
2021-01-23 22:50:02 -0500

    styling

---
+++ src/components/NavToolbar/NavToolbar.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./NavToolbar.css";
+
+const NavToolbar = ({ user }) => {
+  return (
+    <div className="flex nav-toolbar-wrapper rounded-lg pt-2 pb-2 pr-4 pl-4 justify-around box-shadow">
+      <Link to={`/user/${user.uuid}`} className="nav-toolbar-item">
+        <i className="fas fa-user-astronaut"></i>
+      </Link>
+
+      <Link to={`/user/bookmarks`} className="nav-toolbar-item">
+        <i className="fas fa-bookmark"></i>
+      </Link>
+
+      <Link to={`/user/likes`} className="nav-toolbar-item">
+        <i className="fas fa-thumbs-up"></i>
+      </Link>
+    </div>
+  );
+};
+
+export default NavToolbar;
