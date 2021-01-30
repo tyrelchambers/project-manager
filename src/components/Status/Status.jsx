859edf0b9597391b93741e413d33d3352cac6f82
Tyrel Chambers <tychambers3@gmail.com>
2021-01-30 14:45:09 -0500

    edit some styling

--- src/components/Status/Status.jsx
+++ src/components/Status/Status.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Status = ({ text, wrapperClass = "", textClass = "", icon = "" }) => {
   return (
     <p
-      className={`pt-1 pb-1 pl-4 pr-4 font-bold w-fit rounded-full flex items-center ${wrapperClass} ${textClass}`}
+      className={`p-2 font-bold w-fit rounded-lg flex items-center ${wrapperClass} ${textClass}`}
       title={text}
     >
       {icon}
