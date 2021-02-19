b9bcbf67af5a670165cbbe383bc017d7e1d12ae0
Tyrel Chambers <tychambers3@gmail.com>
2021-02-19 18:01:57 -0500

    convert gist to snippet

--- src/pages/Snippets/Snippets.jsx
+++ src/pages/Snippets/Snippets.jsx
@@ -1,18 +1,16 @@
 import React, { useEffect, useState } from "react";
 import DisplayWrapper from "../../layouts/DisplayWrapper/DisplayWrapper";
 import "./Snippets.css";
-import { H1, H2, H2Subtitle } from "../../components/Headings/Headings";
-import { MainButton, MinimalButton } from "../../components/Buttons/Buttons";
+import { H1, H2Subtitle } from "../../components/Headings/Headings";
+import { MinimalButton } from "../../components/Buttons/Buttons";
 import { inject, observer } from "mobx-react";
 import SnippetForm from "../../forms/SnippetForm";
 import { getAxios } from "../../api";
 import SnippetItem from "../../components/SnippetItem/SnippetItem";
-import GistItem from "../../components/GistItem/GistItem";
 
 const Snippets = ({ ModalStore, UserStore }) => {
   const [snippets, setSnippets] = useState([]);
-  const [gists, setGists] = useState([]);
-
+  const [pullingGists, setPullingGists] = useState(false);
   useEffect(() => {
     const fn = async () => {
       await getAxios({
@@ -20,8 +18,6 @@ const Snippets = ({ ModalStore, UserStore }) => {
       }).then((res) => {
         setSnippets(res.snippets);
       });
-
-      getGists();
     };
 
     fn();
@@ -33,16 +29,28 @@ const Snippets = ({ ModalStore, UserStore }) => {
   };
 
   const getGists = async () => {
+    setPullingGists(true);
     const gists = await getAxios({
       url: "/github/gists",
     }).then((res) => res.gists);
 
-    setGists(gists);
+    await getAxios({
+      url: "/github/convert",
+      method: "post",
+      data: {
+        gists,
+      },
+    }).then((res) => {
+      if (res.snippets) {
+        setSnippets([...snippets, ...res.snippets]);
+      }
+    });
+    setPullingGists(false);
   };
   return (
     <DisplayWrapper>
-      <div className="flex justify-between snippets-header">
-        <div className="flex flex-col ">
+      <div className="flex snippets-header">
+        <div className="flex flex-col">
           <H1 className="mr-4">Code Snippets</H1>
           <H2Subtitle>
             Save your favourite functions and share them with others (if you
@@ -51,9 +59,24 @@ const Snippets = ({ ModalStore, UserStore }) => {
           </H2Subtitle>
         </div>
         <div className="w-fit">
-          <MainButton default onClick={addSnippetModelHandler}>
-            Add Snippet
-          </MainButton>
+          {pullingGists ? (
+            <MinimalButton classes="text-yellow-400 mr-4 " onClick={getGists}>
+              <i className="fas fa-sync mr-2 text-sm animate-spin"></i>
+              pulling gists
+            </MinimalButton>
+          ) : (
+            <MinimalButton classes="text-yellow-400 mr-4" onClick={getGists}>
+              <i className="fas fa-sync mr-2 text-sm"></i>
+              pull gists
+            </MinimalButton>
+          )}
+          <MinimalButton
+            classes="text-yellow-400"
+            onClick={addSnippetModelHandler}
+          >
+            <i className="fas fa-plus mr-2 text-sm"></i>
+            add snippet
+          </MinimalButton>
         </div>
       </div>
 
@@ -62,22 +85,6 @@ const Snippets = ({ ModalStore, UserStore }) => {
           snippets.map((snippet, id) => (
             <SnippetItem key={id} snippet={snippet} />
           ))}
-      </div>
-
-      <div className="flex items-center mt-10">
-        <H2 className="mr-8">Github Gists</H2>
-        <MinimalButton classes="text-yellow-400">
-          <i className="fas fa-sync mr-2 text-yellow-400 text-sm"></i>
-          pull gists
-        </MinimalButton>
-      </div>
-      {!UserStore.user.githubAccessToken && (
-        <p>Authenticate with Github to import your Gists</p>
-      )}
-
-      <div className="snippet-list grid grid-cols-5 mt-5 gap-4">
-        {gists.length > 0 &&
-          gists.map((gist, id) => <GistItem key={id} gist={gist} />)}
       </div>
     </DisplayWrapper>
   );
